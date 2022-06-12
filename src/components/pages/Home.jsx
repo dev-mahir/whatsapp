98791dadf9a86ded76465daa9f29cec125231ac0
mdmahir.mk <mdmahir.ds@gmail.com>
2022-06-12 22:02:20 +0600

    whatsapp v1

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <div className="row">
+            <div className="col-lg-3">
+                <div className='user'>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-9"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
